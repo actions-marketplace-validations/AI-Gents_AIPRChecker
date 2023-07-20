0b5edc8319e2beabfe2d3ca5fdb0b01d7095c0fc
carlospolop <carlospolop@gmail.com>
2023-07-20 17:56:41 +0200

    f

--- aiprchecker/AIPRCheckerPrompts.py
+++ aiprchecker/AIPRCheckerPrompts.py
@@ -14,6 +14,8 @@ CHECK_SECURITY_AND_BUGS = """Given the following PR patches check if there are a
 - Code bugs
 - Optimization issues
 - Code smells
+
+If you don't find issues on some of the categories you don't need to mention them, keep the response as short as possible (without missing issues information).
 """
 
 FINAL_CLARIFICATIONS = """Your response must specify the file path and lines affected by each comment and details about why there is an issue."""
